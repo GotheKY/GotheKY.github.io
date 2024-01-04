42b6d45b700a0901cf28b05bdea52e96e235534b
GotheKY <150131456+GotheKY@users.noreply.github.com>
2024-01-04 15:49:26 +0100

    Add files via upload

---
+++ 04_trials.js
@@ -0,0 +1,256 @@
+// In this file you can specify the trial data for your experiment
+
+
+const practice_trials = {
+    key_press: [
+        {
+            picture: "images/practice/13_50_same.jpg",
+            item: 13,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/practice/13_50_different.jpg",
+            item: 13,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/practice/13_150_same.jpg",
+            item: 13,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/practice/13_150_different.jpg",
+            item: 13,
+            expected: "different",
+            angle: 150,
+        },
+        {
+            picture: "images/practice/14_50_same.jpg",
+            item: 14,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/practice/14_50_different.jpg",
+            item: 14,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/practice/14_150_same.jpg",
+            item: 14,
+            expected: "same",
+            angle: 150
+        },
+        
+    ],
+};
+
+const main_trials = {
+    key_press: [
+        {
+            picture: "images/main/1_50_same.jpg",
+            item: 1,
+            expected: "same",
+            angle: 50,
+
+        },
+        {
+            picture: "images/main/1_50_different.jpg",
+            item: 1,
+            expected: "different",
+            angle: 50,
+
+        },
+        {
+            picture: "images/main/1_150_same.jpg",
+            item: 1,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/main/1_150_different.jpg",
+            item: 1,
+            expected: "different",
+            angle: 150,
+        },
+        {
+            picture: "images/main/2_50_same.jpg",
+            item: 2,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/main/2_50_different.jpg",
+            item: 2,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/main/2_150_same.jpg",
+            item: 2,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/main/2_150_different.jpg",
+            item: 2,
+            expected: "different",
+            angle: 150,
+        },
+
+        {
+            picture: "images/main/3_50_same.jpg",
+            item: 3,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/main/3_50_different.jpg",
+            item: 3,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/main/3_150_same.jpg",
+            item: 3,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/main/3_150_different.jpg",
+            item: 3,
+            expected: "different",
+            angle: 150,
+        },
+
+        {
+            picture: "images/main/4_50_same.jpg",
+            item: 4,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/main/4_50_different.jpg",
+            item: 4,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/main/4_150_same.jpg",
+            item: 4,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/main/4_150_different.jpg",
+            item: 4,
+            expected: "different",
+            angle: 150,
+        },
+
+        {
+            picture: "images/main/5_50_same.jpg",
+            item: 5,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/main/5_50_different.jpg",
+            item: 5,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/main/5_150_same.jpg",
+            item: 5,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/main/5_150_different.jpg",
+            item: 5,
+            expected: "different",
+            angle: 150,
+        },
+        {
+            picture: "images/main/6_50_same.jpg",
+            item: 6,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/main/6_50_different.jpg",
+            item: 6,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/main/6_150_same.jpg",
+            item: 6,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/main/6_150_different.jpg",
+            item: 6,
+            expected: "different",
+            angle: 150,
+        },
+
+        {
+            picture: "images/main/7_50_same.jpg",
+            item: 7,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/main/7_50_different.jpg",
+            item: 7,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/main/7_150_same.jpg",
+            item: 7,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/main/7_150_different.jpg",
+            item: 7,
+            expected: "different",
+            angle: 150,
+        },
+
+        {
+            picture: "images/main/8_50_same.jpg",
+            item: 8,
+            expected: "same",
+            angle: 50,
+        },
+        {
+            picture: "images/main/8_50_different.jpg",
+            item: 8,
+            expected: "different",
+            angle: 50,
+        },
+        {
+            picture: "images/main/8_150_same.jpg",
+            item: 8,
+            expected: "same",
+            angle: 150,
+        },
+        {
+            picture: "images/main/8_150_different.jpg",
+            item: 8,
+            expected: "different",
+            angle: 150,
+        },
+
+       
+    ],
+};
